19c46f9b7d8db2353cf4d150d95861597fe858a5
Chinmay <signups@chinmay.design>
2024-11-17 16:12:22 +1100

    Successful Transitions
    Got the swiping transition to work. Working on individual page transitions now.

--- src/features/pageTransitions.js
+++ src/features/pageTransitions.js
@@ -7,22 +7,64 @@ import pageHome from '../pages/page_home'
 import pageWork from '../pages/page_work'
 
 function pageTransitions() {
-  console.log('Page Transitions - Loaded NEW')
+  console.log('Page Transitions - Loaded 14:52')
+
+  gsap.to('.loader', {
+    height: '0%',
+    top: 0,
+    duration: 1,
+    ease: 'power3.inOut',
+  })
 
   barba.init({
     transitions: [
       {
-        name: 'opacity-transition',
+        name: 'swipe-transition',
         leave(data) {
-          return gsap.to(data.current.container, {
-            opacity: 0,
-          })
+          console.log(data)
+          gsap.set('.loader', { bottom: 0, top: 'auto' })
+          return gsap.fromTo(
+            '.loader',
+            {
+              height: '0%',
+            },
+            { height: '100%', duration: 0.75, ease: 'power3.in' }
+          )
         },
         enter(data) {
           data.current.container.remove()
-          return gsap.from(data.next.container, {
-            opacity: 0,
-          })
+
+          const timeline = gsap.timeline()
+
+          timeline.fromTo(
+            '.loader',
+            { bottom: 0, height: '100vh' },
+            {
+              bottom: '100%',
+              duration: 1,
+              ease: 'power3.out',
+            },
+            0.3
+          )
+
+          if (data.next.namespace === 'about') {
+            timeline.fromTo(
+              '.about-image',
+              {
+                opacity: 0,
+                clipPath: 'inset(100% 0 0 0 round 0.25rem)',
+              },
+              {
+                opacity: 1,
+                clipPath: 'inset(0% 0 0 0 round 0.25rem)',
+                duration: 1.5,
+                ease: 'power3.inOut',
+              },
+              0.5
+            )
+          }
+
+          return timeline
         },
       },
     ],
